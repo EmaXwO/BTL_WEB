c9106ff2b51906f520083e3b69e4cb662c028d42
EmaXwO <hoangduy1102002@gmail.com>
2023-05-10 18:36:48 +0700

    Initial commit

---
+++ src/main/java/com/example/btlweb/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.example.btlweb.dto;
+
+import com.example.btlweb.model.Category;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private int categoryId;
+    private double price;
+    private double weight;
+    private String description;
+    private String imageName;
+}
